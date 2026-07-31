f88c8dae36fc81fecbf18d2940153a782a10dce3
Devpratapsingh204 <devpratap284@gmail.com>
2026-07-31 23:18:40 +0530

    Time: 36 ms (81.44%) | Memory: 48.5 MB (23.70%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count =0;
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            int sum =0;
-            for(int j=i; j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
+        unordered_map<long long, int> preSumMap;
+        
+        // Base case: prefix sum of 0 has appeared once (before index 0)
+        preSumMap[0] = 1;
+        
+        long long sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            
+            long long rem = sum - k;
+            
+            // Add all occurrences of (sum - k) to the count
+            if (preSumMap.find(rem) != preSumMap.end()) {
+                count += preSumMap[rem];
             }
+            
+            // Store/increment frequency of current prefix sum
+            preSumMap[sum]++;
         }
+
         return count;
     }
 };
