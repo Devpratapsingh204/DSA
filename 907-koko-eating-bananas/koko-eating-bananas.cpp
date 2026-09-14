7f372dbb2b51b519d7be9a7e4c15eeb9889e1f2e
Dev Pratap Singh <devpratap284@gmail.com>
2026-09-14 17:48:56 +0530

    Time: 13 ms (21.90%) | Memory: 23 MB (24.42%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
-    // Change return type to long long to prevent integer overflow
+    // Function to calculate total hours at given speed
     long long calculateTotalHours(vector<int>& piles, int speed) {
         long long totalH = 0;
+
         for (int bananas : piles) {
-            // Integer division trick equivalent to ceil(bananas / speed)
-            totalH += (bananas + (long long)speed - 1) / speed;
+            totalH += ceil((double)bananas / (double)speed);
         }
+
         return totalH;
     }
 
+    // Function to find minimum eating speed
     int minEatingSpeed(vector<int>& piles, int h) {
+        // Find maximum element
         int maxPile = *max_element(piles.begin(), piles.end());
+
+        // Initialize low and high pointers
         int low = 1, high = maxPile;
         int ans = maxPile;
 
+        // Binary search on answer space
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            // Store result in long long
+            int mid = (low + high) / 2;
+
             long long totalH = calculateTotalHours(piles, mid);
 
+            // If possible, try smaller speed
             if (totalH <= h) {
                 ans = mid;
                 high = mid - 1;
-            } else {
+            }
+            // Otherwise, try larger speed
+            else {
                 low = mid + 1;
             }
         }
+
         return ans;
     }
 };
